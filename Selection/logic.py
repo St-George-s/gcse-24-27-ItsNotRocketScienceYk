d31aa1fe405d6fe64b275b2289d2d4f9aa2e37de
ItsNotRocketScienceYk <AGA001@Stge.org.uk>
2025-09-30 09:25:27 +0000

    if statements and logic

--- Selection/logic.py
+++ Selection/logic.py
@@ -21,9 +21,78 @@
 # elif age >= 18:
 #     print("Oh my mistake, GET IN HEREE")
 
-#QUESTION 2
-age = int(input("HOW OLD ARE YOU, YOU CHIHUAHAU CHILD: "))
-if age >= 18:
-    print("Oh my mistake, GET IN HEREE")
-else:
-    print("I KNEW IT, GET OUTTA HERE WEIRDO")
+# #QUESTION 2
+# age = int(input("HOW OLD ARE YOU, YOU CHIHUAHAU CHILD: "))
+# if age >= 18:
+#     print("You may watch 18+ movies >:]")
+# elif age >= 15:
+#     print("You can watch 15+ movies")
+# else:
+#     print ("You can't watch 15+ or 18+ movies!")
+
+
+
+# #QUESTION 3
+# mood = input("Enter your mood (Happy/Sad/Angry): ")
+# if mood == "happy":
+#     print("You are happy! :) Have a cupcake:")
+# elif mood == "sad":
+#     print("Aww you're sad! Here have a chocolate cake:")
+# elif mood == "angry":
+#     print("AHH YOU'RE ANGRY! >:[ Take this red velvet cake:")
+
+# #QUESTION 4
+# genre = input("Enter your favourite genre (Horror/Fantasy/Action): ")
+# if genre == "horror":
+#     print("You picked horror! Try watching 'The Conjuring'!")
+# elif genre == "fantasy":
+#     print("You picked Fantasy! Try watching 'Lord of the Rings'!")
+# elif genre == "action":
+#     print("You picked Action! Try watching 'Black Panther'!")
+
+
+# #QUESTION 5
+# number1 = float(input("Enter a number: "))
+# number2 = float(input("Input a second number: "))
+# operation = input("Enter an operation (multiply/divide/add/subtract)")
+# if operation == "multiply":
+#     print(number1 * number2)
+# elif operation == "divide":
+#     print(number1/number2)
+# elif operation == "add":
+#     print(number1 + number2)
+# elif operation == "subtract":
+#     print(number1 - number2)
+
+#LOGICCCCCCCCCCCCCC
+
+# #Question 1
+# weather = input("Enter weather (sunny/rainy): ")
+# temperature = float(input("Enter a temperature: "))
+# if weather == "sunny" and temperature > 15:
+#     print("GO TOUCH GRASS YOU GAMER RAT")
+# else:
+#     print("GO READ BOOKS YOU NERD")
+
+# #Question 2
+# weather = input("Enter weather (sunny/rainy): ")
+# temperature = float(input("Enter a temperature: "))
+# if weather == "rainy" or temperature > 15:
+#     print("Read. Do it.")
+# else:
+#     print("Go outside. Lazy.")
+
+# #Question 3
+# apples = int(input("Enter the number of apples harvested: "))
+# if apples < 10:
+#     print("Awful harvest! Try again :[")
+# else:
+#     print("Good job! Excellent harvest! :]")
+
+#Question 4
+age = int(input("Enter your age: "))
+driversLicense = input("Do you have a drivers license (True/False)")
+if age > 18 and driversLicense == "true":
+    print("You can drive! ;]")
+else: 
+    print("You can't drive! Hands off the wheel! >:[")
