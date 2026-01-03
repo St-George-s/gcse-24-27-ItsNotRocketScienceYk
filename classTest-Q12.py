defe4df56951ba30beca6b98c1a5adff6d821ab5
ItsNotRocketScienceYk <AGA001@Stge.org.uk>
2026-01-03 00:46:11 +0000

    vjggvk

--- classTest-Q12.py
+++ classTest-Q12.py
@@ -1,8 +1,14 @@
 # Question 12 - Santa's elves
-import random
-for counter in range(1,11):
-    randScore = random.randint(1,100)
-        if randScore > 50:
-                print("Child:",counter, "Your score is", randScore, "Good! You will recieve a gift :)")
-                    elif randScore <= 50:
-                            print("Child:",counter, "Your score is", randScore, "Naughty! You will not recieve a gift :(")
+import random 
+
+for counter in range(1,11): 
+
+    randScore = random.randint(1,100) 
+
+    if randScore > 50: 
+
+        print("Child:",counter, "Your score is", randScore, "Good! You will recieve a gift :)") 
+
+    elif randScore <= 50: 
+
+        print("Child:",counter, "Your score is", randScore, "Naughty! You will not recieve a gift :(") 
