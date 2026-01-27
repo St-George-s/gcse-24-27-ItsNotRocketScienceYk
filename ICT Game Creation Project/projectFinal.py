69767a1d7b24337709c0abbc9c1765f655d52b07
ItsNotRocketScienceYk <AGA001@Stge.org.uk>
2026-01-27 10:25:26 +0000

    vfndkjvn

--- ICT Game Creation Project/projectFinal.py
+++ ICT Game Creation Project/projectFinal.py
@@ -10,11 +10,11 @@ while not valid:
     lowercase = 0
     uppercase = 0
     
-    for count in password:
-        if count.islower():
-            lowercase += 1
-        if count.isupper():
-            uppercase += 1
+    for char in password:
+        if ord(str(char)) >= 65 and ord(str(char)) <= 90:
+            uppercase = uppercase + 1
+        if ord(str(char)) >= 97 and ord(str(char)) <= 122:
+           lowercase = lowercase + 1
     
     if len(password) >= 8 and lowercase > 0 and uppercase > 0:
         print("Password has been reset :)")
@@ -26,3 +26,13 @@ while not valid:
             print("Password does not contain lowercase letters.")
         if uppercase == 0:
             print("Password does not contain UPPERCASE letters.")
+
+specialCharFound = False 
+for char in password:
+    asciiVal= ord(char)
+    if (asciiVal >= 33 and asciiVal <= 47) or (asciiVal >= 58 and asciiVal <= 64) or (asciiVal >= 91 and asciiVal <= 96) and (asciiVal>= 123 and asciiVal <= 126):
+        specialCharFound = True
+print(specialCharFound)
+
+if specialCharFound and len(password) > 12:
+    print("Password is strong :)")
