1bc2943fa1becd1b5ba5daaa07df5cfeb407fc43
ItsNotRocketScienceYk <AGA001@Stge.org.uk>
2026-02-03 10:21:13 +0000

    liyana 3/2/25

--- ICT Game Creation Project/mastermind.py
+++ ICT Game Creation Project/mastermind.py
@@ -1,5 +1,17 @@
 import random
 number = random.randint(1000, 9999)
 guess = input("Enter a 4-digit guess: ")
-while
-#print("The random number is:", number)
+
+while guess != number:
+    if len(guess) != 4:
+        print("4 NUMBERS ONLY YOU PRICK")
+        guess = input("Try again, 4-digits only!: ")
+    elif len(guess) == 4:
+        for count in range(1, 4):
+            print("guess is wrong, try again!")
+            guess = input("Enter a 4-digit guess: ")
+    print (number, "is right answer!")
+print("Well done! You guessed the correct number :3")
+    
+
+#print"The random number is:", number
