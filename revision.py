8271daf6e9ceeaf5d5aa65c023801b35b9423a16
ItsNotRocketScienceYk <AGA001@Stge.org.uk>
2026-04-27 19:59:46 +0000

    27/07/2026

--- revision.py
+++ revision.py
@@ -1,2 +1,37 @@
 password = 0
 username = ""
+
+## IF STATEMENTS --- SELECTION STATEMENT
+# x = input("Enter passcode: ")
+
+# if x == "GO":
+#     print("Access granted")
+# else:
+#     print("Access denied")
+
+# x = input("Enter passcode: ")
+# usertype = input("Enter usertype: ")
+
+# if x == "GO":
+#     if usertype == "teacher":
+#         print("Unrestricted access granted")
+#     else:
+#         print("Restricted access granted")
+# else:
+#     print("Access denied")
+
+## - Arrays
+x = int(input("Enter number: "))
+list = [1, 2, 3, 4, 5]
+
+Match = False
+
+for i in range(0, len(list)):
+    print(list[i])
+    if x == list[i]:
+        Match = True
+
+if Match == True:
+    print("Number found!")
+else:
+    print("Number not found...")
