836fa7ea57c8c403f38a8d5d313aead0e510217e
ItsNotRocketScienceYk <AGA001@Stge.org.uk>
2026-04-28 00:55:07 +0000

    28/04/2026

--- File handling/createFile.py
+++ File handling/createFile.py
@@ -2,10 +2,10 @@
 # with open("holiday.txt", "w") as file:
 #     file.write("I re-decorated my room! :D")
 
-# #Question 2 - Write a Python program to read and print the contents of a text file. 
-# with open("/workspaces/gcse-24-27-ItsNotRocketScienceYk/File handling/holiday.txt", "r") as file:
-#     liyana = file.read()
-#     print(liyana)
+#Question 2 - Write a Python program to read and print the contents of a text file. 
+with open("/workspaces/gcse-24-27-ItsNotRocketScienceYk/File handling/holiday.txt", "r") as file:
+    liyana = file.read()
+    print(liyana)
 
 # #Question 3 - Write a Python program to append a new line of text to an existing file.
 # with open("/workspaces/gcse-24-27-ItsNotRocketScienceYk/File handling/holiday.txt", "a") as file:
--- revision.py
+++ revision.py
@@ -1,5 +1,3 @@
-password = 0
-username = ""
 
 ## IF STATEMENTS --- SELECTION STATEMENT
 # x = input("Enter passcode: ")
@@ -21,17 +19,57 @@ username = ""
 #     print("Access denied")
 
 ## - Arrays
-x = int(input("Enter number: "))
-list = [1, 2, 3, 4, 5]
+# x = int(input("Enter number: "))
+# list = [1, 2, 3, 4, 5]
 
-Match = False
+# Match = False
 
-for i in range(0, len(list)):
-    print(list[i])
-    if x == list[i]:
-        Match = True
+# for i in range(0, len(list)):
+#     print(list[i])
+#     if x == list[i]:
+#         Match = True
 
-if Match == True:
-    print("Number found!")
-else:
-    print("Number not found...")
+# if Match == True:
+#     print("Number found!")
+# else:
+#     print("Number not found...")
+
+
+# x = ["S", "T", "R", "I", "N", "G"]
+# y = ["S, t, r, i, n, g"]
+# z = "sTrInG"
+
+# print(z[-2:])
+
+##String Manipulation
+# surname = input("State your surname")
+# age = str(input("State your age (2 digits): "))
+# town = input("Town: ")
+
+# #first 3 letters of town in uppercase + age as 2 digits + first and last letter of your surname
+# username = town[0:3].upper() + age + surname[0].lower() + surname[len(surname) - 1].lower()
+
+# print(username)
+
+# word = input("Input a word: ")
+
+# firstLetter = word[0]
+# #first character removed and the last character replaced with the original first character
+# newWord = word[1:len(word)-1] + firstLetter
+
+# print(newWord)
+
+# b = int(input("Enter a number: "))
+# x = 1
+# while x <= 100:
+#     print(b*x)
+#     x = x + 1
+
+# age = int(input("Enter your age: "))
+
+# for i in range(age):
+#     print("happy birthday)
+
+x = int("123")
+y=x*10
+print(x)
